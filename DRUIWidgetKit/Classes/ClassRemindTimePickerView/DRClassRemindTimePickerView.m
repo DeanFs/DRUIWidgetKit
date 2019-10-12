503e69c03584682fbd7b1b0082d45d66ca3a0341
Dean_F <247159603@qq.com>
2019-10-12 22:50:42 +0800

    fix

--- DRUIWidgetKit/Classes/ClassRemindTimePickerView/DRClassRemindTimePickerView.m
+++ DRUIWidgetKit/Classes/ClassRemindTimePickerView/DRClassRemindTimePickerView.m
@@ -37,56 +37,6 @@
 
     self.pickerView.dataSource = @[dayList, @[@""], hourList, minuteList];
     self.pickerView.currentSelectedStrings = @[dayString, @"", hourString, minuteString];
-
-    kDRWeakSelf
-    self.pickerView.onSelectedChangeBlock = ^(NSInteger section, NSInteger index, NSString *selectedString) {
-        if (section == 0) {
-            weakSelf.isThisDay = (BOOL)index;
-        } else if (section == 2) {
-            weakSelf.hour = index;
-        } else if (section == 3) {
-            weakSelf.minute = index * weakSelf.timeScale;
-        }
-    };
-    self.pickerView.getFontForSectionWithBlock = ^UIFont *(NSInteger section) {
-        if (section == 0) {
-            return [UIFont dr_PingFangSC_MediumWithSize:17];
-        }
-        if (section == 1) {
-            return [UIFont systemFontOfSize:6];
-        }
-        return [UIFont dr_PingFangSC_RegularWithSize:26];
-    };
-    self.pickerView.getWidthForSectionWithBlock = ^CGFloat(NSInteger section) {
-        if (section == 0) {
-            return 100;
-        }
-        if (section == 1) {
-            return 14;
-        }
-        return 60;
-    };
-    self.pickerView.getTextAlignmentForSectionBlock = ^NSTextAlignment(NSInteger section) {
-        if (section == 0) {
-            return NSTextAlignmentRight;
-        }
-        if (section == 2) {
-            return NSTextAlignmentRight;
-        }
-        return NSTextAlignmentLeft;
-    };
-    self.pickerView.getSeparateTextBeforeSectionBlock = ^NSString *(NSInteger section) {
-        if (section == 2) {
-            return @"/";
-        }
-        return @"";
-    };
-    self.pickerView.getIsLoopForSectionBlock = ^BOOL(NSInteger section) {
-        if (section == 0 || section == 1) {
-            return NO;
-        }
-        return YES;
-    };
 }
 
 #pragma mark - setup xib
@@ -113,13 +63,60 @@
 
 - (void)setup {
     if (!self.pickerView) {
-        DRNormalDataPickerView *picker = [[DRNormalDataPickerView alloc] init];
-        [self addSubview:picker];
-        [picker mas_makeConstraints:^(MASConstraintMaker *make) {
+        kDRWeakSelf
+        DRNormalDataPickerView *pickerView = [[DRNormalDataPickerView alloc] init];
+        pickerView.onSelectedChangeBlock = ^(NSInteger section, NSInteger index, NSString *selectedString) {
+            if (section == 0) {
+                weakSelf.isThisDay = (BOOL)index;
+            } else if (section == 2) {
+                weakSelf.hour = index;
+            } else if (section == 3) {
+                weakSelf.minute = index * weakSelf.timeScale;
+            }
+        };
+        pickerView.getFontForSectionWithBlock = ^UIFont *(NSInteger section) {
+            if (section == 0) {
+                return [UIFont dr_PingFangSC_MediumWithSize:17];
+            }
+            if (section == 1) {
+                return [UIFont systemFontOfSize:6];
+            }
+            return [UIFont dr_PingFangSC_RegularWithSize:26];
+        };
+        pickerView.getWidthForSectionWithBlock = ^CGFloat(NSInteger section) {
+            if (section == 1) {
+                return 14;
+            }
+            return 60;
+        };
+        pickerView.getTextAlignmentForSectionBlock = ^NSTextAlignment(NSInteger section) {
+            if (section == 0) {
+                return NSTextAlignmentRight;
+            }
+            if (section == 2) {
+                return NSTextAlignmentRight;
+            }
+            return NSTextAlignmentLeft;
+        };
+        pickerView.getSeparateTextBeforeSectionBlock = ^NSString *(NSInteger section) {
+            if (section == 2) {
+                return @"/";
+            }
+            return @"";
+        };
+        pickerView.getIsLoopForSectionBlock = ^BOOL(NSInteger section) {
+            if (section == 0 || section == 1) {
+                return NO;
+            }
+            return YES;
+        };
+        self.pickerView = pickerView;
+        self.isThisDay = NO;
+
+        [self addSubview:self.pickerView];
+        [self.pickerView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.left.bottom.right.mas_offset(0);
         }];
-        self.pickerView = picker;
-        self.isThisDay = NO;
     }
 }
 
